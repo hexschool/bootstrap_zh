e04873e297e70d885ff462bca8cf7999438c2cdd
wcc723 <agito723@gmail.com>
2018-07-10 11:33:04 +0800

    Update 2018-07-10T03:33:01.252Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
-var gulp = require('gulp'),
-  ghPages = require('gulp-gh-pages');
+let gulp = require('gulp'),
+  ghPages = require('gulp-gh-pages')
 
-gulp.task('deploy', function() {
-  return gulp.src('./_gh_pages/**/*')
-    .pipe(ghPages({
-      remoteUrl: 'https://github.com/hexschool/bootstrap_zh.git'
-    }));
-});
+gulp.task('deploy', () => gulp.src('./_gh_pages/**/*')
+  .pipe(ghPages({
+    remoteUrl: 'https://github.com/hexschool/bootstrap_zh.git'
+  })))
